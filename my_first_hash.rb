e0ed262b8b702c2db6d53f3cb99a74c269d30aba
Marcos Rodriguez <m.rodrig.felix@gmail.com>
2020-03-13 04:03:38 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -2,6 +2,7 @@ def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
   my_hash = { "fist_name" => "Marcos", "last_name" => "Rodriguez"}
+
 end
 
 
@@ -12,6 +13,7 @@ def shipping_manifest
 
   the_manifest = { "whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3 }
   the_manifest
+
 end
 
 def retrieval
@@ -23,6 +25,7 @@ def retrieval
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
   shipping_manifest["oil paintings"]
+
 end
 
 def adding
